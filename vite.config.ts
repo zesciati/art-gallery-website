a58ea7ca6a137eb7dadc98681bbc0003c5ec48de
zesciati <zakyabiyyu2003@gmail.com>
2026-04-20 11:18:59 +0700

    changing it

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cloudflare()],
   resolve:{
     alias:{
       "@": path.resolve(__dirname, "./src")
